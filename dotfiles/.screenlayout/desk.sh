5ce76da41922d0c95a924914443c1ee4649c68be
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2022-05-09 10:22:21 +0200

    x: Fix desk screenlayout script

--- dotfiles/.screenlayout/desk.sh
+++ dotfiles/.screenlayout/desk.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --output HDMI-1 --off --output DP-2-2 --off
+xrandr -s 0
+sleep 3
+xrandr --output eDP-1 --primary --mode 1920x1080 \
+       --output HDMI-1 --off \
+       --output DP-2-1 --off \
+       --output DP-2-2 --off \
+       --output DP-2-3 --off
+#xrandr --output eDP-1 --primary --mode 1920x1080 --output HDMI-1 --off --output DP-2-2 --off
 
-sleep 1
+sleep 3
+xrandr --output eDP-1 --off \
+       --output DP-2-1 --primary --mode 1920x1080 --rotate normal
+sleep 3
 
-xrandr --output DP-2-1 --off --output DP-2-2 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-2-3 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+xrandr --output DP-2-1 --mode 3840x2160 --pos 1920x0 --rotate normal \
+       --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal
