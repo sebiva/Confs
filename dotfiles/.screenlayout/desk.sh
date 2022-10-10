d5d6a88ebc53ab5755e46343d7cf2d3e445b9a2e
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2022-10-10 16:31:09 +0200

    x: Change desk to HDMI

--- dotfiles/.screenlayout/desk.sh
+++ dotfiles/.screenlayout/desk.sh
@@ -2,16 +2,8 @@
 xrandr -s 0
 sleep 3
 xrandr --output eDP-1 --primary --mode 1920x1080 \
-       --output HDMI-1 --off \
-       --output DP-2-1 --off \
-       --output DP-2-2 --off \
-       --output DP-2-3 --off
-#xrandr --output eDP-1 --primary --mode 1920x1080 --output HDMI-1 --off --output DP-2-2 --off
-
-sleep 3
-xrandr --output eDP-1 --off \
-       --output DP-2-1 --primary --mode 1920x1080 --rotate normal
+       --output HDMI-1 --off
 sleep 3
 
-xrandr --output DP-2-1 --mode 3840x2160 --pos 1920x0 --rotate normal \
+xrandr --output HDMI-1 --mode 3840x2160 --pos 1920x0 --rotate normal \
        --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal
