77aaf9e58d50a9551a456088c0040558d198ec86
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2024-01-02 15:48:55 +0100

    x: Update homescreen layout

--- dotfiles/.screenlayout/homeoffice.sh
+++ dotfiles/.screenlayout/homeoffice.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-xrandr --output HDMI-1 --mode 2560x1440 --pos 1920x0 --rotate normal \
-       --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal
+xrandr --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal \
+       --output eDP-1 --primary --mode 1920x1080 --pos 2560x360 --rotate normal
+
